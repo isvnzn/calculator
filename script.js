50757b9091c860427fe4508fa363582e83c79710
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-22 04:21:21 +0800

    Add operate func

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const add = (aOperand, bOperand) => {
-  return aOperand + bOperand;
+let aOperand = 0;
+let bOperand = 0;
+let operator;
+
+const add = (aNum, bNum) => {
+  return aNum + bNum;
 };
 
-const subtract = (aOperand, bOperand) => {
-  return aOperand - bOperand;
+const subtract = (aNum, bNum) => {
+  return aNum - bNum;
 };
 
-const multiply = (aOperand, bOperand) => {
-  return aOperand * bOperand;
+const multiply = (aNum, bNum) => {
+  return aNum * bNum;
 };
 
-const divide = (aOperand, bOperand) => {
-  return aOperand / bOperand;
+const divide = (aNum, bNum) => {
+  return aNum / bNum;
+};
+
+const operate = (aNum, bNum, operator) => {
+  switch (operator) {
+    case "+":
+      add(aNum, bNum);
+      break;
+    case "-":
+      subtract(aNum, bNum);
+      break;
+    case "*":
+      multiply(aNum, bNum);
+      break;
+    case "/":
+      divide(aNum, bNum);
+      break;
+    default:
+      console.log("Something went wrong.");
+      break;
+  }
 };
